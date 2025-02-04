e9b0571f3ecff32d34183da3a448012983075226
Damian Rovara <damianrovara@gmail.com>
2025-02-04 18:40:35 +0100

    feat: :sparkles: add config parsing functionality and tests

--- src/commit/__init__.py
+++ src/commit/__init__.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 from . import commits
+from .config import Config, NewCommitType, NewGitmoji, parse_config
 from .main import main
 
-__all__ = ["commits", "main"]
+__all__ = ["Config", "NewCommitType", "NewGitmoji", "commits", "main", "parse_config"]
